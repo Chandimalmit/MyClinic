d4e7a8e39b906a05e2b79e3a1796f7e41d981236
Udesh Amarathunga <harischandrax@gmail.com>
2018-02-15 09:51:04 +0530

    add all

--- register.php
+++ register.php
@@ -19,7 +19,7 @@
 	<form action="controllers/register_controller.php" method="post">
 		<fieldset>
 		 <div class="form-group">	
-		<label>Title</label>
+		<label class="pull-left">Title</label>
 			<select name="title">
 				<option value="mr">Please Select Title</option>
 				<option value="Dr">Dr</option>
@@ -31,22 +31,22 @@
 	    </div>
 
         <div class="form-group">
-		    <label>First Name</label>
+		    <label class="pull-left">First Name</label>
 			<input type="text" name="firstname" required />
 	    </div>
 
         <div class="form-group">
-		    <label>Middle Name</label>
+		    <label class="pull-left">Middle Name</label>
 			<input type="text" name="middlename" />
 	    </div>
 
          <div class="form-group">
-		    <label>Last Name</label>
+		    <label class="pull-left">Last Name</label>
 			<input type="text" name="lastname" required />
         </div>
         
         <div class="form-group">
-        	<label>Gender</label>
+        	<label class="pull-left">Gender</label>
 	        <select name="gender" required>
 	        	<option>Male</option>
 	        	<option>Female</option>
@@ -54,42 +54,42 @@
         </div>
 
         <div class="form-group">
-        	 <label>Date of Birth</label>
+        	 <label class="pull-left">Date of Birth</label>
         	<input type="date" name="date" id="datepicker" required />
         </div>
 
         <div class="form-group">
-        	<label>NIC Number</label>
+        	<label class="pull-left">NIC Number</label>
         	<input type="text" name="nic_name" required />
         </div>
 
         <div class="form-group">
-        	<label>Clinic Number</label>
+        	<label class="pull-left">Clinic Number</label>
         	<input type="text" name="clinic_number" required />
         </div>
 
          <div class="form-group">
-        	<label>Permanant Address</label>
+        	<label class="pull-left">Permanant Address</label>
         	<input type="text" name="p_address" required />
         </div>
 
          <div class="form-group">
-        	<label>Home Phone Number</label>
+        	<label class="pull-left">Home Phone Number</label>
         	<input type="text" name="home_phone_number" />
         </div>
 
         <div class="form-group">
-        	<label>Mobile Phone Number</label>
+        	<label class="pull-left">Mobile Phone Number</label>
         	<input type="text" name="mobile_form_number" />
         </div> 
 
         <div class="form-group">
-        	<label>Email Address</label>
+        	<label class="pull-left">Email Address</label>
         	<input type="email" name="email" required />
         </div>
 
          <div class="form-group">
-        	<label>Remarks</label>
+        	<label class="pull-left">Remarks</label>
         	<textarea name="remarks"></textarea>
         </div>
 
